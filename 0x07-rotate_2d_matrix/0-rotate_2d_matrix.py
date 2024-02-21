ea09096e6d32216ebc1ce94c5152d1569f2fa964
KevCareSA <kev.mkbn2@gmail.com>
2024-02-21 11:54:54 +0200

    Updated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+
+"""
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place. 
+    """ rotates a matrix in its place
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    # first reverse the matrix
+    matrix.reverse()
+    n = len(matrix)
+    # then proceed to swap indices
+    for i in range(n):
+        for j in range(n):
+            if j > i:
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+    matrix2 = [[1, 2, 3, 4],
+               [5, 6, 7, 8],
+               [9, 10, 11, 12],
+               [13, 14, 15, 16]]
+    rotate_2d_matrix(matrix)
+    rotate_2d_matrix(matrix2)
+    print(matrix)
+    print(matrix2)
